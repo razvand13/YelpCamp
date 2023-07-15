0b44f5f71451d316244676ac79821ecb1f27fcad
razvand13 <R.D.Dinu-1@student.tudelft.nl>
2023-07-15 16:41:54 +0300

    Feat: Basic CRUD functionality

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
-const { readdirSync } = require('fs');
+const methodOverride = require('method-override')
 
 // no extra options needed here as of Mongoose 6 
 // https://www.mongodb.com/community/forums/t/option-usecreateindex-is-not-supported/123048
@@ -22,6 +22,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -45,6 +46,23 @@ app.post('/campgrounds', async (req, res) => {
 app.get('/campgrounds/:id', async(req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {campground});
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {campground});
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
 })
 
 app.listen(3000, () => {
